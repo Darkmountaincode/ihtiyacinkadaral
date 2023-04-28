552eee354fb43d6f00a468bdb7c91f4731a42dcb
Darkmountaincode <129421868+Darkmountaincode@users.noreply.github.com>
2023-04-28 15:15:42 +0300

    Update api.js

--- api/api.js
+++ api/api.js
@@ -13,7 +13,4 @@ const deleteCategory = async (categoryId) => {
  
     }
   }
-  // google dan girin 2 dk google den nereye gircem hocam google mı açayım  heh tmm discordu açtım verin kullanıcı adınızı :D maybelatersource#0984 ☆ Can#8830
-  //49141545 hocam yarın denesek ya bu terslik de bir iş var ama D:
-  /// sorun ney hocam  ustam ne yaparsam yapayım şu hatayı alıyoru m
-  // dc den ekran açar mısınız dc nedir discord475162795 any desk versem :D eskin hocam valla bunun icinde 8 9 saatir uğraşmayı geçtim konu bile açtım 3 forma 2dk nı almaz aslında :d discord kurayım ben istersen :d
+  
